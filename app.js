723ed28313d3288a172ffc5f03c1ee5b995e9ce9
Sinane <sin21.sim42@gmail.com>
2020-02-13 14:03:51 +0100

    suppr node modules

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 io.listen(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('receiveSentence', "test");
-  socket.on('sendSentence', msg => {
+io.on("connection", socket => {
+  console.log("a user connected");
+  io.emit("receiveSentence", "test");
+  socket.on("sendSentence", msg => {
     console.log("receive: ", msg);
-    io.emit('receiveSentence', msg);
+    io.emit("receiveSentence", msg);
   });
 });
 
 server.listen(5001, () => {
-  console.log('listen on 5001 ...');
+  console.log("listen on 5001 ...");
 });
