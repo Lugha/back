6628e20ecfd596e5fa1ec5c5cd0acf79e42c483c
Jalil Taibi <jaliltaibi@MacBook-Pro-de-Manel.local>
2020-02-13 17:00:51 +0100

    c cool

--- app.js
+++ app.js
@@ -6,14 +6,39 @@ const io = require('socket.io')(server);
 
 io.listen(server);
 
+const sentencesAndTraductions = [
+  {
+    sentence: 'How are you today ?',
+    traductions: [
+      'Il pleut aujourd\'hui ?',
+      '*Comment allez-vous aujourd\'hui ?',
+      'La glace était-elle délicieuse ?'
+    ]
+  },
+  {
+    sentence: 'Can you give me food ?',
+    traductions: [
+      '*Tu peux me donner à manger ?',
+      'Pourquoi t\'es nul à smash ?',
+      'Je serai musclé dans combien de temps ?'
+    ]
+  },
+]
+
+let i = 0;
+function sendSentence() {
+  console.log("sending to mobiles");
+  io.sockets.emit('receiveSentence', JSON.stringify(sentencesAndTraductions[i]));
+  if (i === 0) i = 1;
+  else if (i === 1) i = 0;
+  setTimeout(sendSentence, 5 * 1000);
+}
+
 io.on('connection', (socket) => {
   console.log('a user connected');
-  io.emit('receiveSentence', "test");
-  socket.on('sendSentence', msg => {
-    console.log("receive: ", msg);
-    io.emit('receiveSentence', msg);
-  });
 });
+
+sendSentence();
 
 server.listen(5001, () => {
   console.log('listen on 5001 ...');
